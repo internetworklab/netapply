213c7f761ff274537c1989a0e3ebdbd7186178b7
root <root@wien1.exploro.one>
2025-10-13 18:17:12 +0000

    working on vrf and routes object

---
+++ pkg/interface/route/method.go
@@ -0,0 +1,164 @@
+package route
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"strings"
+
+	pkgdocker "github.com/internetworklab/netapply/pkg/docker"
+	"github.com/internetworklab/netapply/pkg/interface/vrf"
+	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
+)
+
+func (scope RouteScope) ToUInt8() netlink.Scope {
+	switch scope {
+	case RouteScopeUniverse:
+		return unix.RT_SCOPE_UNIVERSE
+	case RouteScopeSite:
+		return unix.RT_SCOPE_SITE
+	case RouteScopeLink:
+		return unix.RT_SCOPE_LINK
+	case RouteScopeHost:
+		return unix.RT_SCOPE_HOST
+	case RouteScopeNowhere:
+		return unix.RT_SCOPE_NOWHERE
+	default:
+		return unix.RT_SCOPE_UNIVERSE
+	}
+}
+
+func (r *RouteConfig) GetInterfaceName() string {
+	return r.Destionation
+}
+
+func (r *RouteConfig) GetContainerName() *string {
+	return r.ContainerName
+}
+
+// If it returns nil, it means that the table(or vrf) is not created yet
+func (r *RouteConfig) GetTableId(ctx context.Context) (*uint32, error) {
+	type result struct {
+		TableId *uint32
+	}
+	res := new(result)
+	res.TableId = new(uint32)
+	*res.TableId = 0
+
+	if r.TableId != nil {
+		return r.TableId, nil
+	}
+
+	if r.VRF != nil && *r.VRF != vrf.VRFNameDefault && *r.VRF != vrf.VRFNameEmpty {
+		err := pkgdocker.WithNsHandle(ctx, r.ContainerName, func(handle *netlink.Handle) error {
+			vrfLink, err := handle.LinkByName(*r.VRF)
+			if err != nil {
+				return fmt.Errorf("failed to get vrf link: %w", err)
+			}
+			if v, ok := vrfLink.(*netlink.Vrf); ok {
+				var tableId uint32 = v.Table
+				res.TableId = &tableId
+				return nil
+			} else {
+				return fmt.Errorf("link is not a vrf link: %v, probably because the name is used by other types of links (e.g., a dummy, a veth, or a bridge,)", vrfLink)
+			}
+
+		})
+		if err != nil {
+			return nil, err
+		}
+		return res.TableId, nil
+	}
+
+	return res.TableId, nil
+}
+
+func (r *RouteConfig) CheckExist(ctx context.Context) (bool, error) {
+	tableId, err := r.GetTableId(ctx)
+	if err != nil {
+		return false, fmt.Errorf("the route might specified a vrf, but failed to get table id: %w", err)
+	}
+
+	if tableId == nil {
+		return false, fmt.Errorf("the route might specified a vrf, but table id is not available yet")
+	}
+
+	type result struct {
+		Exist bool
+	}
+	res := new(result)
+
+	err = pkgdocker.WithNsHandleSafe(ctx, r.ContainerName, func(handle *netlink.Handle) error {
+		destination, err := netlink.ParseAddr(r.Destionation)
+		if err != nil {
+			return fmt.Errorf("failed to parse destination: %w", err)
+		}
+		routes, err := handle.RouteGet(destination.IP)
+		if err != nil {
+			return fmt.Errorf("failed to get routes: %w", err)
+		}
+
+		protoExpected := RouteProtocolStatic.ToInt()
+		if r.Protocol != nil {
+			protoExpected = r.Protocol.ToInt()
+		}
+		for _, nlroute := range routes {
+			if nlroute.Protocol == protoExpected {
+				res.Exist = true
+				break
+			}
+		}
+
+		return nil
+	})
+
+	return res.Exist, err
+}
+
+func (protocol RouteProtocol) ToInt() netlink.RouteProtocol {
+	checkFiles := []string{
+		"/etc/iproute2/rt_protos", // /etc/xyz always takes precedence over /usr/share/xyz since the later is (mostly) vendor-shipped, and the former reflects the intention of the sysadmin.
+		"/usr/share/iproute2/rt_protos",
+	}
+	for _, fname := range checkFiles {
+		fcontentsBytes, err := os.ReadFile(fname)
+		if err != nil {
+			log.Printf("Warining, when reading file %s: %v", fname, err)
+			continue
+		}
+
+		fcontents := string(fcontentsBytes)
+		lines := strings.Split(fcontents, "\n")
+		for _, line := range lines {
+			tline := strings.TrimSpace(line)
+			if tline == "" {
+				continue
+			}
+			if strings.HasPrefix(tline, "#") {
+				continue
+			}
+			parts := strings.Split(tline, " ")
+			if len(parts) < 2 {
+				continue
+			}
+
+			protocolId, err := strconv.Atoi(parts[0])
+			if err != nil {
+				continue
+			}
+
+			for i := 1; i < len(parts); i++ {
+				if parts[i] == string(protocol) {
+					return netlink.RouteProtocol(protocolId)
+				}
+			}
+		}
+	}
+
+	// In most cases, 0 means unspecified or default value
+	return netlink.RouteProtocol(0)
+}
--- pkg/interface/route/types.go
+++ pkg/interface/route/types.go
@@ -1,11 +1,27 @@
 package route
 
+type RouteScope string
+
+const (
+	RouteScopeUniverse RouteScope = "universe"
+	RouteScopeSite     RouteScope = "site"
+	RouteScopeLink     RouteScope = "link"
+	RouteScopeHost     RouteScope = "host"
+	RouteScopeNowhere  RouteScope = "nowhere"
+)
+
+type RouteProtocol string
+
+const (
+	RouteProtocolStatic RouteProtocol = "static"
+)
+
 type RouteConfig struct {
-	Name    string `yaml:"name" json:"name"`
-	TableId *int   `yaml:"table_id,omitempty" json:"table_id,omitempty"`
+	TableId *uint32 `yaml:"table_id,omitempty" json:"table_id,omitempty"`
 
 	// When TableId and VRF are both specified, TableId will take precedence over VRF
 	// If they are both nil, the default VRF will be used
+	// In practice, we recommend you to use table id directly as possible as you can, instead of VRF.
 	VRF *string `yaml:"vrf,omitempty" json:"vrf,omitempty"`
 
 	ContainerName *string `yaml:"container_name,omitempty" json:"container_name,omitempty"`
@@ -16,6 +32,22 @@ type RouteConfig struct {
 	NextHop string `yaml:"next_hop,omitempty" json:"next_hop,omitempty"`
 
 	NextHopInterface *string `yaml:"next_hop_interface,omitempty" json:"next_hop_interface,omitempty"`
+
+	// Source address to prefer for the route
+	Source *string `yaml:"source,omitempty" json:"source,omitempty"`
+
+	// See https://pkg.go.dev/github.com/vishvananda/netlink#Scope
+	// If omitted, it use RouteScopeUniverse for default
+	Scope *RouteScope `yaml:"scope,omitempty" json:"scope,omitempty"`
+
+	Priority *int `yaml:"priority,omitempty" json:"priority,omitempty"`
+	Family   *int `yaml:"family,omitempty" json:"family,omitempty"`
+
+	// See /etc/iproute2/rt_protos
+	// It indicates who installed the route to the routing table, currently,
+	// only 'static' is supported since this program is expected to be invoked (or indirectly invoked) by the sysadmin.
+	// And when reconciling, we use this field to differentiate it from the others (such as those installed by BGP or OSPF)
+	Protocol *RouteProtocol `yaml:"protocol,omitempty" json:"protocol,omitempty"`
 }
 
 type RouteConfigurationList []RouteConfig
