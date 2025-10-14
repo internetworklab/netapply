76a9480b3067014878737c0cecb3c569d5f46f7e
root <root@wien1.exploro.one>
2025-10-14 18:40:10 +0000

    daemonize

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ import (
 	"github.com/docker/docker/client"
 	"gopkg.in/yaml.v3"
 
+	"encoding/json"
+	"net/http"
+
 	pkgdocker "github.com/internetworklab/netapply/pkg/docker"
 	pkgmodels "github.com/internetworklab/netapply/pkg/models"
 	pkgutils "github.com/internetworklab/netapply/pkg/utils"
@@ -81,48 +84,61 @@ func getGlobalConfig(configPath string, clientAuth *pkgutils.ClientAuth) (*pkgmo
 
 // CLI structure for Kong
 type CLI struct {
-	Up   UpCmd   `cmd:"" help:"Start the service with the specified configuration"`
-	Down DownCmd `cmd:"" help:"Stop all containers associated with the service"`
+	Up                    UpCmd         `cmd:"" help:"Start the service with the specified configuration"`
+	Down                  DownCmd       `cmd:"" help:"Stop all containers associated with the service"`
+	ServeLocal            ServeLocalCmd `cmd:"" help:"Serving as a local configurator"`
+	ServiceName           string        `required:"" help:"Name of the service" short:"s"`
+	Node                  string        `required:"" help:"Name of the node to start" short:"n"`
+	TLSTrustedCACert      string        `help:"Path to trusted CA certificate file for TLS" type:"path"`
+	TLSClientCert         string        `help:"Path to client certificate file for TLS" type:"path"`
+	TLSClientKey          string        `help:"Path to client private key file for TLS" type:"path"`
+	HTTPBasicAuthUsername string        `help:"Username for HTTP basic authentication"`
+	HTTPBasicAuthPassword string        `help:"Password for HTTP basic authentication"`
 }
 
 type UpCmd struct {
-	Config                string `required:"" help:"Path to the configuration file"`
-	ServiceName           string `required:"" help:"Name of the service" short:"s"`
-	Node                  string `required:"" help:"Name of the node to start" short:"n"`
-	TLSTrustedCACert      string `help:"Path to trusted CA certificate file for TLS" type:"path"`
-	TLSClientCert         string `help:"Path to client certificate file for TLS" type:"path"`
-	TLSClientKey          string `help:"Path to client private key file for TLS" type:"path"`
-	HTTPBasicAuthUsername string `help:"Username for HTTP basic authentication"`
-	HTTPBasicAuthPassword string `help:"Password for HTTP basic authentication"`
+	Config string `required:"" help:"Path to the configuration file"`
 }
 
 type DownCmd struct {
-	ServiceName string `required:"" help:"Name of the service" short:"s"`
 }
 
-// Run method for UpCmd
-func (cmd *UpCmd) Run() error {
-	ctx := context.Background()
+type ServeLocalCmd struct {
+	BindUnixSocket string `help:"Path to the unix socket to bind" type:"path"`
+}
 
+func initCtx(ctx context.Context, globalCLIConfig *CLI) (context.Context, error) {
 	// Initialize Docker client
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		return fmt.Errorf("failed to create docker client: %w", err)
+		return ctx, fmt.Errorf("failed to create docker client: %w", err)
 	}
 	defer cli.Close()
 
 	clientAuth := &pkgutils.ClientAuth{
-		TLSClientCertFile:     cmd.TLSClientCert,
-		TLSClientKeyFile:      cmd.TLSClientKey,
-		TLSTrustedCACertFile:  cmd.TLSTrustedCACert,
-		HTTPBasicAuthUsername: cmd.HTTPBasicAuthUsername,
-		HTTPBasicAuthPassword: cmd.HTTPBasicAuthPassword,
+		TLSClientCertFile:     globalCLIConfig.TLSClientCert,
+		TLSClientKeyFile:      globalCLIConfig.TLSClientKey,
+		TLSTrustedCACertFile:  globalCLIConfig.TLSTrustedCACert,
+		HTTPBasicAuthUsername: globalCLIConfig.HTTPBasicAuthUsername,
+		HTTPBasicAuthPassword: globalCLIConfig.HTTPBasicAuthPassword,
 	}
 
 	// Set up context with service name and docker client
-	ctx = pkgutils.SetServiceNameInCtx(ctx, cmd.ServiceName)
+	ctx = pkgutils.SetServiceNameInCtx(ctx, globalCLIConfig.ServiceName)
 	ctx = pkgutils.SetDockerCliInCtx(ctx, cli)
 	ctx = pkgutils.SetClientAuthInCtx(ctx, clientAuth)
+	return ctx, nil
+}
+
+// Run method for UpCmd
+func (cmd *UpCmd) Run(globalCliConfigAny interface{}) error {
+	ctx := context.Background()
+	globalCLIConfig := globalCliConfigAny.(*CLI)
+	ctx, err := initCtx(ctx, globalCLIConfig)
+	if err != nil {
+		return fmt.Errorf("failed to initialize context: %w", err)
+	}
+	clientAuth, _ := pkgutils.ClientAuthFromCtx(ctx)
 
 	// Read and parse the configuration
 	var globalConfig *pkgmodels.GlobalConfig
@@ -131,13 +147,13 @@ func (cmd *UpCmd) Run() error {
 	}
 
 	// Get the specified node configuration
-	nodeConfig, ok := globalConfig.Nodes[cmd.Node]
+	nodeConfig, ok := globalConfig.Nodes[globalCLIConfig.Node]
 	if !ok {
-		return fmt.Errorf("node '%s' not found in configuration", cmd.Node)
+		return fmt.Errorf("node '%s' not found in configuration", globalCLIConfig.Node)
 	}
 
 	// Start the service
-	log.Printf("Setting up service %s on node %s ...", cmd.ServiceName, cmd.Node)
+	log.Printf("Setting up service %s on node %s ...", globalCLIConfig.ServiceName, globalCLIConfig.Node)
 	if err := nodeConfig.Up(ctx); err != nil {
 		return fmt.Errorf("failed to start service: %w", err)
 	}
@@ -146,7 +162,10 @@ func (cmd *UpCmd) Run() error {
 }
 
 // Run method for DownCmd
-func (cmd *DownCmd) Run() error {
+func (cmd *DownCmd) Run(globalConfig interface{}) error {
+	globalCLIConfig := globalConfig.(*CLI)
+	serviceName := globalCLIConfig.ServiceName
+
 	ctx := context.Background()
 
 	// Initialize Docker client
@@ -157,7 +176,7 @@ func (cmd *DownCmd) Run() error {
 	defer cli.Close()
 
 	// Set up context with service name and docker client
-	ctx = pkgutils.SetServiceNameInCtx(ctx, cmd.ServiceName)
+	ctx = pkgutils.SetServiceNameInCtx(ctx, serviceName)
 	ctx = pkgutils.SetDockerCliInCtx(ctx, cli)
 
 	// Stop all containers associated with the service
@@ -165,14 +184,73 @@ func (cmd *DownCmd) Run() error {
 		return fmt.Errorf("failed to stop service: %w", err)
 	}
 
-	log.Printf("Service '%s' stopped successfully\n", cmd.ServiceName)
+	log.Printf("Service '%s' stopped successfully\n", serviceName)
+	return nil
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func respondError(w http.ResponseWriter, err error, code int) {
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(ErrorResponse{Error: err.Error()})
+}
+
+func (cmd *ServeLocalCmd) Run(globalConfig interface{}) error {
+
+	globalCLIConfig := globalConfig.(*CLI)
+	ctx, err := initCtx(context.Background(), globalCLIConfig)
+	if err != nil {
+		return fmt.Errorf("failed to initialize context: %w", err)
+	}
+
+	log.Printf("Serving as a local configurator on %s\n", cmd.BindUnixSocket)
+	return nil
+
+	server := &http.Server{
+		Addr: cmd.BindUnixSocket,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("Received request: %s %s", r.Method, r.URL.Path)
+
+			nodeConfig := new(pkgmodels.NodeConfig)
+
+			contentType := r.Header.Get("Content-Type")
+			var err error
+
+			if strings.HasPrefix(contentType, "application/yaml") {
+				err = yaml.NewDecoder(r.Body).Decode(nodeConfig)
+			} else if strings.HasPrefix(contentType, "application/json") {
+				err = json.NewDecoder(r.Body).Decode(nodeConfig)
+			} else {
+				err = json.NewDecoder(r.Body).Decode(nodeConfig)
+			}
+
+			if err != nil {
+				respondError(w, err, http.StatusBadRequest)
+				return
+			}
+
+			if err := nodeConfig.Up(ctx); err != nil {
+				respondError(w, err, http.StatusBadRequest)
+				return
+			}
+
+			w.WriteHeader(http.StatusOK)
+		}),
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		return fmt.Errorf("failed to listen and serve: %w", err)
+	}
+
 	return nil
 }
 
 func main() {
 	var cli CLI
 	ctx := kong.Parse(&cli)
-	err := ctx.Run()
+	err := ctx.Run(&cli)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
