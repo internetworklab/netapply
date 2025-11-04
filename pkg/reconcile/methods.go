0dfd8bed61b57aac3252d217fbf942ca31fce601
root <root@wien1.exploro.one>
2025-11-04 09:25:13 +0000

    keep simplifying

--- pkg/reconcile/methods.go
+++ pkg/reconcile/methods.go
@@ -19,69 +19,78 @@ func (dpChangeSet *ResourceListChangeSet) Merge(other *ResourceListChangeSet) *R
 
 	result := new(ResourceListChangeSet)
 
-	mergedAdded := make(map[string][]ResourceProvisioner)
+	mergedAdded := make(map[string]ResourceProvisioner)
+	mergedUpdated := make(map[string]InterfaceChangeSet)
+	mergedRemoved := make(map[string]ResourceCanceller)
+
 	for k, v := range dpChangeSet.AddedResources {
-		mergedAdded[k] = append(mergedAdded[k], v...)
+		mergedAdded[k+":"+v.GetType()] = v
 	}
 	for k, v := range other.AddedResources {
-		if curr, ok := mergedAdded[k]; ok {
-			mergedAdded[k] = append(curr, v...)
-		} else {
-			mergedAdded[k] = v
-		}
+		mergedAdded[k+":"+v.GetType()] = v
 	}
 
-	mergedUpdated := make(map[string][]InterfaceChangeSet)
 	for k, v := range dpChangeSet.UpdatedResources {
-		mergedUpdated[k] = append(mergedUpdated[k], v...)
+		mergedUpdated[k+":"+v.GetType()] = v
 	}
 	for k, v := range other.UpdatedResources {
-		if curr, ok := mergedUpdated[k]; ok {
-			mergedUpdated[k] = append(curr, v...)
-		} else {
-			mergedUpdated[k] = v
-		}
+		mergedUpdated[k+":"+v.GetType()] = v
 	}
 
-	mergedRemoved := make(map[string][]ResourceCanceller)
 	for k, v := range dpChangeSet.RemovedResources {
-		mergedRemoved[k] = append(mergedRemoved[k], v...)
+		mergedRemoved[k+":"+v.GetType()] = v
 	}
 	for k, v := range other.RemovedResources {
-		if curr, ok := mergedRemoved[k]; ok {
-			mergedRemoved[k] = append(curr, v...)
-		} else {
-			mergedRemoved[k] = v
-		}
+		mergedRemoved[k+":"+v.GetType()] = v
 	}
 
 	result.AddedResources = mergedAdded
 	result.UpdatedResources = mergedUpdated
 	result.RemovedResources = mergedRemoved
-
 	return result
 }
 
 func (dpChangeSet *ResourceListChangeSet) HasChanges() bool {
-	if dpChangeSet != nil {
-		for _, addedInterface := range dpChangeSet.AddedResources {
-			if len(addedInterface) > 0 {
-				return true
+	if dpChangeSet == nil {
+		return false
+	}
+	return len(dpChangeSet.RemovedResources)+len(dpChangeSet.UpdatedResources)+len(dpChangeSet.AddedResources) > 0
+}
+
+func reOrderAddedInterfaces(provisioners map[string]ResourceProvisioner) []ResourceProvisioner {
+	results := make([]ResourceProvisioner, 0)
+	for _, provisioner := range provisioners {
+		results = append(results, provisioner)
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return getOrderByType(results[i].GetType()) < getOrderByType(results[j].GetType())
+	})
+
+	return results
+}
+
+func (dpChangeSet *ResourceListChangeSet) Apply(ctx context.Context) error {
+	if dpChangeSet.HasChanges() {
+		for _, canceller := range dpChangeSet.RemovedResources {
+			if err := canceller.Cancel(ctx); err != nil {
+				return fmt.Errorf("failed to cancel interface: %w", err)
 			}
 		}
-		for _, updatedInterface := range dpChangeSet.UpdatedResources {
-			if len(updatedInterface) > 0 {
-				return true
+
+		for _, changeSet := range dpChangeSet.UpdatedResources {
+			if err := changeSet.Apply(ctx); err != nil {
+				return fmt.Errorf("failed to update interface: %w", err)
 			}
 		}
-		for _, removedInterface := range dpChangeSet.RemovedResources {
-			if len(removedInterface) > 0 {
-				return true
+
+		for _, provisioner := range reOrderAddedInterfaces(dpChangeSet.AddedResources) {
+			if err := provisioner.Create(ctx); err != nil {
+				return fmt.Errorf("failed to create interface: %w", err)
 			}
 		}
 	}
-
-	return false
+	return nil
 }
 
 func getOrderByType(ty string) int16 {
@@ -110,106 +119,3 @@ func getOrderByType(ty string) int16 {
 
 	return defaultOrder
 }
-
-func reOrderAddedInterfaces(provisioners map[string][]ResourceProvisioner) []ResourceProvisioner {
-	results := make([]ResourceProvisioner, 0)
-	for _, provisioner := range provisioners {
-		results = append(results, provisioner...)
-	}
-
-	sort.Slice(results, func(i, j int) bool {
-		return getOrderByType(results[i].GetType()) < getOrderByType(results[j].GetType())
-	})
-
-	return results
-}
-
-func (dpChangeSet *ResourceListChangeSet) Apply(ctx context.Context) error {
-	if dpChangeSet.HasChanges() {
-		for _, removedInterface := range dpChangeSet.RemovedResources {
-			for _, canceller := range removedInterface {
-				if err := canceller.Cancel(ctx); err != nil {
-					return fmt.Errorf("failed to cancel interface: %w", err)
-				}
-			}
-		}
-
-		for _, updatedInterface := range dpChangeSet.UpdatedResources {
-			for _, changeSet := range updatedInterface {
-				if changeSet.HasUpdates() {
-					if err := changeSet.Apply(ctx); err != nil {
-						return fmt.Errorf("failed to update interface: %w", err)
-					}
-				}
-			}
-		}
-
-		for _, provisioner := range reOrderAddedInterfaces(dpChangeSet.AddedResources) {
-			if err := provisioner.Create(ctx); err != nil {
-				return fmt.Errorf("failed to create interface: %w", err)
-			}
-		}
-	}
-	return nil
-}
-
-// func (dpChangeSet *ResourceListChangeSet) Log() {
-// 	if dpChangeSet == nil {
-// 		return
-// 	}
-
-// 	rows := make([]table.Row, 0)
-// 	for _, ifaces := range dpChangeSet.AddedResources {
-// 		for _, iface := range ifaces {
-// 			rows = append(rows,
-// 				table.Row{
-// 					"Added",
-// 					pkgutils.GetContainerDisplayName(iface.GetContainerName()),
-// 					iface.GetInterfaceName(),
-// 					iface.GetType(),
-// 				},
-// 			)
-// 		}
-// 	}
-
-// 	for _, ifaces := range dpChangeSet.RemovedResources {
-// 		for _, iface := range ifaces {
-// 			rows = append(rows,
-// 				table.Row{
-// 					"Removed",
-// 					pkgutils.GetContainerDisplayName(iface.GetContainerName()),
-// 					iface.GetInterfaceName(),
-// 					iface.GetType(),
-// 				},
-// 			)
-// 		}
-// 	}
-
-// 	for _, ifaces := range dpChangeSet.UpdatedResources {
-// 		for _, iface := range ifaces {
-// 			rows = append(rows,
-// 				table.Row{
-// 					"Updated",
-// 					pkgutils.GetContainerDisplayName(iface.GetContainerName()),
-// 					iface.GetInterfaceName(),
-// 					iface.GetType(),
-// 				},
-// 			)
-// 		}
-// 	}
-
-// 	// table with some amount of customization
-// 	tw := table.NewWriter()
-// 	// append a header row
-// 	tw.AppendHeader(table.Row{"Direction", "Container", "Resource", "Type"})
-// 	// append some data rows
-// 	tw.AppendRows(rows)
-// 	tw.SetStyle(table.StyleLight)
-// 	// customize the style and change some stuff
-// 	tw.Style().Format.Header = text.FormatLower
-// 	tw.Style().Format.Row = text.FormatLower
-// 	tw.Style().Format.Footer = text.FormatLower
-// 	tw.Style().Options.SeparateColumns = false
-// 	// render it
-// 	fmt.Println(tw.Render())
-// }
