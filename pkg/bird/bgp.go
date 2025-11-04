87796c5029071fbdf7947448d219adddfe60641f
root <root@wien1.exploro.one>
2025-11-04 07:14:06 +0000

    add bson struct tags

--- pkg/bird/bgp.go
+++ pkg/bird/bgp.go
@@ -13,24 +13,24 @@ import (
 )
 
 type BGPProtocol struct {
-	Name         string  `yaml:"name" json:"name"`
-	Template     *string `yaml:"template,omitempty" json:"template,omitempty"`
-	Interface    *string `yaml:"interface,omitempty" json:"interface,omitempty"`
-	LocalAddress *string `yaml:"local_address,omitempty" json:"local_address,omitempty"`
-	PeerAddress  *string `yaml:"peer_address,omitempty" json:"peer_address,omitempty"`
-	LocalASN     *string `yaml:"local_asn,omitempty" json:"local_asn,omitempty"`
-	PeerASN      *string `yaml:"peer_asn,omitempty" json:"peer_asn,omitempty"`
-	PeerExternal *bool   `yaml:"peer_external,omitempty" json:"peer_external,omitempty"`
-	PeerInternal *bool   `yaml:"peer_internal,omitempty" json:"peer_internal,omitempty"`
+	Name         string  `yaml:"name" json:"name" bson:"name"`
+	Template     *string `yaml:"template,omitempty" json:"template,omitempty" bson:"template,omitempty"`
+	Interface    *string `yaml:"interface,omitempty" json:"interface,omitempty" bson:"interface,omitempty"`
+	LocalAddress *string `yaml:"local_address,omitempty" json:"local_address,omitempty" bson:"local_address,omitempty"`
+	PeerAddress  *string `yaml:"peer_address,omitempty" json:"peer_address,omitempty" bson:"peer_address,omitempty"`
+	LocalASN     *string `yaml:"local_asn,omitempty" json:"local_asn,omitempty" bson:"local_asn,omitempty"`
+	PeerASN      *string `yaml:"peer_asn,omitempty" json:"peer_asn,omitempty" bson:"peer_asn,omitempty"`
+	PeerExternal *bool   `yaml:"peer_external,omitempty" json:"peer_external,omitempty" bson:"peer_external,omitempty"`
+	PeerInternal *bool   `yaml:"peer_internal,omitempty" json:"peer_internal,omitempty" bson:"peer_internal,omitempty"`
 
 	// For storing in database, distinguish which node the resource belongs to.
-	Node *string `yaml:"node,omitempty" json:"node,omitempty"`
+	Node *string `yaml:"node,omitempty" json:"node,omitempty" bson:"node,omitempty"`
 
 	// For storing in database, the unique ID to distinguish the resource in the global scope.
-	ResourceID *string `yaml:"resource_id,omitempty" json:"resource_id,omitempty"`
+	ResourceID *string `yaml:"resource_id,omitempty" json:"resource_id,omitempty" bson:"resource_id,omitempty"`
 
 	// To support soft-deletion.
-	Deleted bool `yaml:"deleted,omitempty" json:"deleted,omitempty"`
+	Deleted bool `yaml:"deleted,omitempty" json:"deleted,omitempty" bson:"deleted,omitempty"`
 }
 
 const patternWildCard = "*"
